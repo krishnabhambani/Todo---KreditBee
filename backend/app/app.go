580a70a6f9bfec38cfcdd604e828c7d14c46b432
krishnabhambani <krishnabhambani1@gmail.com>
2026-06-17 16:30:46 +0530

    Updated docker file

---
+++ backend/app/app.go
@@ -0,0 +1,17 @@
+package app
+
+import (
+	"github.com/todo-app/backend/config"
+)
+
+type App struct {
+	appConfig config.Config
+}
+
+
+func NewApp(appConfig config.Config) *App {
+	return &App{
+		appConfig: appConfig
+	}
+}
+
